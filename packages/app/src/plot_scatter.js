5fbf5611de101b2b6a6ee694b9322867bc536a50
Josh Dando <joshdando@Joshs-MacBook-Pro.local>
2021-10-03 17:47:26 +0100

    plot the hypothesis line

--- packages/app/src/plot_scatter.js
+++ packages/app/src/plot_scatter.js
@@ -1,3 +1,5 @@
+import meanSquareError from "../utils/mse";
+
 export default async function plotScatterDiagram(id, url) {
   // set the dimensions and margins of the graph
   const margin = { top: 10, right: 30, bottom: 30, left: 60 },
@@ -19,10 +21,10 @@ export default async function plotScatterDiagram(id, url) {
   ).then(function (data) {
       console.log("data: ", data);
     // Add X axis
-    const xs = data.map(d => d["X2 house age"])
+    const xs = data.map(d => +d["X2 house age"])
     const max_x = Math.max(...xs)
 
-    const ys = data.map(d => d["Y house price of unit area"])
+    const ys = data.map(d => +d["Y house price of unit area"])
     const max_y = Math.max(...ys)
 
     const x = d3.scaleLinear().domain([0, max_x]).range([0, width]);
@@ -49,5 +51,31 @@ export default async function plotScatterDiagram(id, url) {
       })
       .attr("r", 1.5)
       .style("fill", "#69b3a2");
-  });
-}
+      // Add the regression line
+      // linear line
+      // const hypothesisData = [{ x: 0, y: height}, { x: width, y: 0}];
+  
+      // TODO: calculate the error for the hypothesis using mse
+      // y = mx + c
+      // using linear line so h = f(x)
+      const y_intercept = 20;
+      const y_hats = data.map(d => parseFloat(d["X2 house age"]) + y_intercept)
+      console.log("y_hats: ", y_hats)
+      const err = meanSquareError(y_hats, ys)
+      console.log("MSE: ", err)
+
+      const hypothesisData = data.map((d, i) => ({ x: d["X2 house age"], y: y_hats[i] }))
+    
+      const lineFunc = d3.line()
+      .x( d => x(d.x) )
+      .y( d => y(d.y) )
+    
+      svg.append('path')
+      .datum(hypothesisData)
+      .attr('d', lineFunc(hypothesisData))
+      .attr('stroke', 'blue')
+      .attr('fill', 'none');
+
+    });
+    
+  }
--- packages/app/utils/mse.js
+++ packages/app/utils/mse.js
@@ -1,7 +1,7 @@
 // y_hat is the predicted y value
 // y is the actual value
 // returns: the mean square error
-function meanSquareError(y_hat, y) {
+export default function meanSquareError(y_hat, y) {
     if (y.length != y_hat.length) {
         throw new Error("length of predicted values doesnt match output values length")
     }
@@ -9,7 +9,5 @@ function meanSquareError(y_hat, y) {
 	for (let i = 0; i < y.length; i++) {
 		error += Math.pow((y_hat[i] - y[i]), 2)
 	}
-	return error / a.length
+	return error / y.length
 }
-
-module.exports = meanSquareError;
