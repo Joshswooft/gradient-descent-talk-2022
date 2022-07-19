c2f6920359cdf1d3243357215438f9bd749a81a2
Josh Dando <joshdando@Joshs-MacBook-Pro.local>
2022-07-19 15:41:08 +0100

    wip: grad descent

---
+++ packages/app/utils/gradient_descent.js
@@ -0,0 +1,13 @@
+export default function gradient_descent() {
+    // given a hypothesis theta_0, theta_1
+    // optimise the cost function in this case MSE
+
+    theta_0 = 0;
+    theta_1 = 1;
+    learning_rate = 0.1;
+    let err = 10000000;
+    // repeat until convergence
+    while (err > 10) {
+        t0 = theta_0 - learning_rate * 
+    }
+}
