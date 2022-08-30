d4a25ac667a1402acba365c607b4a0a8648682af
Josh Dando <joshdando@MWG44P29PY.local>
2022-08-30 21:38:09 +0100

    feat: style the cost function page

--- packages/app/src/pages/costfunction.svelte
+++ packages/app/src/pages/costfunction.svelte
@@ -1,4 +1,6 @@
 <script>
+  import CodeBlock from "../components/CodeBlock.svelte";
+  import SurfacePlot from "../components/SurfacePlot.svelte";
   import { mse, modifiedMse } from "../utils/equations";
 </script>
 
@@ -8,25 +10,32 @@
   </script>
 </svelte:head>
 
-<h1>Cost function</h1>
-<ul>
-  <li>Allows models to analyze how well it fits the data.</li>
-  <li>Returns the cost which is the degree of error.</li>
-  <li>Greater the cost => more error</li>
-</ul>
-<p>
-  The formula that we will be using as our cost function is known as mean
-  squared error.
-</p>
-<p>$$ {mse} $$</p>
-<span>n = number of training examples</span>
-<p>
-  One thing to note about MSE is that it does not perform well with outliers -
-  This is the result of squaring each term, which effectively weights large
-  errors more heavily than small ones.
-</p>
-<p>$$ {modifiedMse} $$</p>
-<p>
-  We use a slightly modified MSE (notice the 2n term). The reason for this is it
-  makes the maths easier when it comes to differentiation.
-</p>
+<article class="prose">
+  <h1 class="mb-8 border-b-2">Cost function</h1>
+  <ul>
+    <li>Allows models to analyze how well it fits the data.</li>
+    <li>Returns the cost which is the degree of error.</li>
+    <li>Greater the cost => more error</li>
+  </ul>
+  <p>
+    The formula that we will be using as our cost function is known as mean
+    squared error.
+  </p>
+  <CodeBlock>
+    <p>$$ {mse} $$</p>
+    <span>where n = number of training examples</span>
+  </CodeBlock>
+  <p>
+    One thing to note about MSE is that it does not perform well with outliers -
+    This is the result of squaring each term, which effectively weights large
+    errors more heavily than small ones.
+  </p>
+  <CodeBlock>
+    <p>$$ {modifiedMse} $$</p>
+  </CodeBlock>
+  <p>
+    We use a slightly modified MSE (notice the 2n term). The reason for this is
+    it makes the maths easier when it comes to differentiation.
+  </p>
+  <SurfacePlot />
+</article>
